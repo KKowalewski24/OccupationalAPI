406e539e4064519bfb235ace10f42d853c9171e6
Kamil Kowalewski <szamil128@onet.pl>
2020-02-18 19:36:57 +0100

    Initial commit

---
+++ src/main/java/pl/kkowalewski/occupationalapi/OccupationalApiApplication.java
@@ -0,0 +1,13 @@
+package pl.kkowalewski.occupationalapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OccupationalApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OccupationalApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/kkowalewski/occupationalapi/OccupationalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kkowalewski.occupationalapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OccupationalApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
