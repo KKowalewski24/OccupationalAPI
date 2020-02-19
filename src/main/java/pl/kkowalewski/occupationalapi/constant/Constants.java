ea1a89280d6a6f3e0e817302151e42e931cbf220
Kamil Kowalewski <szamil128@onet.pl>
2020-02-19 19:05:59 +0100

    Add SwaggerConfiguration

--- src/main/java/pl/kkowalewski/occupationalapi/constant/Constants.java
+++ src/main/java/pl/kkowalewski/occupationalapi/constant/Constants.java
@@ -1,12 +1,15 @@
 package pl.kkowalewski.occupationalapi.constant;
 
-public abstract class Constants {
+public class Constants {
 
     /*------------------------ FIELDS REGION ------------------------*/
     public static final String UNDERSCORE_ID = "_id";
     public static final String UNDERSCORE_FK = "_fk";
     public static final String LOWER_CASE_DEVELOPER = "developer";
     public static final String LOWER_CASE_CLIENT = "client";
+
+    public static final String HOME = "";
+    public static final String SLASH = "/";
 
     /*------------------------ METHODS REGION ------------------------*/
     private Constants() {
