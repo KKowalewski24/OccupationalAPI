4b9583ca8c50500319901c9c4700212ca0496722
Kamil Kowalewski <szamil128@onet.pl>
2020-02-19 13:38:41 +0100

    Add connection to postgres + add relation between Developer and Client + add loader

--- src/main/java/pl/kkowalewski/occupationalapi/model/entity/client/Client.java
+++ src/main/java/pl/kkowalewski/occupationalapi/model/entity/client/Client.java
@@ -2,21 +2,26 @@ package pl.kkowalewski.occupationalapi.model.entity.client;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
 import pl.kkowalewski.occupationalapi.model.base.Person;
 import pl.kkowalewski.occupationalapi.model.entity.developer.Developer;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import java.time.LocalDate;
 
 @Getter
 @Setter
-@Entity
+@Entity(name = "clients")
 public class Client extends Person {
 
     /*------------------------ FIELDS REGION ------------------------*/
-    //    TODO ADD PARAMETERS TO RELATION
+    // TODO CHECK IF CORRECT
     @ManyToOne
+    @JoinColumn(name = "developer_id")
     private Developer developer;
 
     /*------------------------ METHODS REGION ------------------------*/
@@ -30,6 +35,40 @@ public class Client extends Person {
     public Client(String firstName, String lastName, LocalDate birthDate, Developer developer) {
         super(firstName, lastName, birthDate);
         this.developer = developer;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Client client = (Client) o;
+
+        return new EqualsBuilder()
+                .appendSuper(super.equals(o))
+                .append(developer, client.developer)
+                .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37)
+                .appendSuper(super.hashCode())
+                .append(developer)
+                .toHashCode();
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .appendSuper(super.toString())
+                .append("developer", developer)
+                .toString();
     }
 }
     
--- src/main/java/pl/kkowalewski/occupationalapi/model/entity/developer/Developer.java
+++ src/main/java/pl/kkowalewski/occupationalapi/model/entity/developer/Developer.java
@@ -2,22 +2,26 @@ package pl.kkowalewski.occupationalapi.model.entity.developer;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
 import pl.kkowalewski.occupationalapi.model.base.Person;
 import pl.kkowalewski.occupationalapi.model.entity.client.Client;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.OneToMany;
 import java.time.LocalDate;
 import java.util.List;
 
 @Getter
 @Setter
-@Entity
+@Entity(name = "developers")
 public class Developer extends Person {
 
     /*------------------------ FIELDS REGION ------------------------*/
-    //    TODO ADD PARAMETERS TO RELATION
-    @OneToMany
+    // TODO CHECK IF CORRECT
+    @OneToMany(fetch = FetchType.EAGER, mappedBy = "developer")
     private List<Client> clientList;
 
     /*------------------------ METHODS REGION ------------------------*/
@@ -32,6 +36,40 @@ public class Developer extends Person {
                      List<Client> clientList) {
         super(firstName, lastName, birthDate);
         this.clientList = clientList;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Developer developer = (Developer) o;
+
+        return new EqualsBuilder()
+                .appendSuper(super.equals(o))
+                .append(clientList, developer.clientList)
+                .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37)
+                .appendSuper(super.hashCode())
+                .append(clientList)
+                .toHashCode();
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .appendSuper(super.toString())
+                .append("clientList", clientList)
+                .toString();
     }
 }
     
