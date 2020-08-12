d8a6879c7ad8972d5f45e61aa3087cad38e54e0c
Kamil Kowalewski <szamil24@gmail.com>
2020-08-12 21:54:16 +0200

    Improve enum

--- src/main/java/pl/kkowalewski/occupationalapi/model/Type.java
+++ src/main/java/pl/kkowalewski/occupationalapi/model/Type.java
@@ -1,17 +1,38 @@
 package pl.kkowalewski.occupationalapi.model;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public enum Type {
 
+    /*------------------------ FIELDS REGION ------------------------*/
     FRONT_END("Front-end"),
     BACK_END("Back-end");
 
-    private String type;
+    private final String name;
 
-    Type(String type) {
-        this.type = type;
+    /*------------------------ METHODS REGION ------------------------*/
+    Type(String name) {
+        this.name = name;
     }
 
-    public String getType() {
-        return type;
+    public String getName() {
+        return name;
+    }
+
+    public static Type fromString(final String text) {
+        return Arrays.asList(Type.values())
+                .stream()
+                .filter((it) -> it.getName().equals(text))
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new);
+    }
+
+    public static List<String> getNamesList() {
+        return Arrays.asList(Type.values())
+                .stream()
+                .map((it) -> it.getName())
+                .collect(Collectors.toList());
     }
 }
